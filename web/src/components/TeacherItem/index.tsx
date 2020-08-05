c020e6821ed280d26fac054b9e2007fed27fd71b
vcwild <vcwildm@gmail.com>
2020-08-05 18:27:50 -0300

    update components

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,34 +1,56 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
-import './styles.css'
+import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
         <header>
-          <img src="https://avatars3.githubusercontent.com/u/49621396?s=460&u=287b710177f3025e48914f95368aa057eff6db5b&v=4" alt="Victor Wildner"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Victor Wildner</strong>
-            <span>Química</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
 
-        <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br /><br />
-          Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
           <p>
             Preço/Hora 
-            <strong>R$ 80.00</strong>
+            <strong>R$ {teacher.cost}</strong>
           </p>
-          <button type="button">
+          <a 
+            target="_blank" 
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`}
+          >
             <img src={whatsappIcon} alt="WhatsApp"/>
             Entrar em contato
-          </button>
+          </a>
         </footer>
       </article>
   )
