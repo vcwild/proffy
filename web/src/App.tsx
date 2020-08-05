c25dfdae26c39afd19e7bafa500de50d413ccd0c
vcwild <vcwildm@gmail.com>
2020-08-05 18:28:48 -0300

    update app routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import Routes from './routes';
+
 import './assets/styles/global.css';
 
-import Landing from './pages/Landing'
-import Routes from './routes';
 
 function App() {
   return (
